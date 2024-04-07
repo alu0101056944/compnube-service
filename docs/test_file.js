bc82eb8f4bbf5daecfa95ade3560cd2c90ce1285
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2024-04-07 23:22:21 +0100

    Logic finished. fix error pending.

---
+++ docs/test_file.js
@@ -0,0 +1,4 @@
+function main() {
+  console.log('Client script executed.');
+  return 2 + 3;
+}
--- web-server/app.js
+++ web-server/app.js
@@ -17,6 +17,9 @@ import expressAppCreator from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import process from 'process';
+import { readFile } from 'fs/promises';
+
+import formidable, { errors as formidableErrors } from 'formidable';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -26,15 +29,35 @@ const __dirname = path.dirname(__filename);
  */
 function main() {
   const application = expressAppCreator();
+
   application.set('port', 8080);
+
   const PATH_TO_ROOT = path.join(__dirname, '../www');
   const PATH_TO_SRC = path.join(__dirname, '../src');
   application.use(expressAppCreator.static(PATH_TO_ROOT));
   application.use(expressAppCreator.static(PATH_TO_SRC));
+
   application.listen(application.get('port'), '0.0.0.0', function() {
     const DEFAULT_START_MESSAGE =
         'The server is running on http://<your machine IP addr>:';
     console.log(DEFAULT_START_MESSAGE + application.get('port'));
+  });
+
+  application.post('/compute', async (request, response) => {
+    const form = formidable({});
+    try {
+        const files = await form.parse(request);
+        const FILE_CONTENT = await readFile(files.file[0].filepath, 'utf-8');
+        // ... validate js, check for main(), execute main(), receive result and send back
+
+        response.writeHead(200, { 'Content-Type': 'text/plain' });
+        response.end(String(FILE_CONTENT));
+    } catch (err) {
+        console.error(err);
+        response.writeHead(err.httpCode || 400, { 'Content-Type': 'text/plain' });
+        response.end(String(err));
+        return;
+    }
   });
 }
 
